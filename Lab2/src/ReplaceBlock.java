8bdbc8009a65af8912a9e3529bf5983980f4c02e
Eistern <zulind@inbox.ru>
2019-02-24 19:23:45 +0700

    Homework 24.02

--- Lab2/src/ReplaceBlock.java
+++ Lab2/src/ReplaceBlock.java
@@ -3,7 +3,8 @@ public class ReplaceBlock implements BlockInterface {
     public String[] run(String[] input) throws Exception {
         if (input == null || input.length != 3)
             throw new Exception("Invalid number of arguments");
-        return null;
+
+        return new String[]{input[2].replaceAll(input[0], input[1])};
     }
     //Input - Text, Output - Text
     //2 necessary args and text
